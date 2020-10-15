6eac90c478df37d77f5933275bdf143164d2fac9
Den <den34092den@gmail.com>
2020-10-15 18:00:35 +0300

    Upload

--- Lab_1/6/6.cpp
+++ Lab_1/6/6.cpp
@@ -1,8 +1,7 @@
 ﻿#include<stdio.h>
 using namespace std;
 int main() {
-	short time = 0;
-	long long time1 = 0;
-	time = 100 * 366 * 24 * 60 * 60;
-	time1 = 100 * 366 * 24 * 60 * 60;
+	int time = 0, time1 = 0;
+	time = 10 * 366 * 24 * 60 * 60; // no overflow
+	time1 = 100 * 366 * 24 * 60 * 60; // overflow
 }
