9055d9cea2855d941b6cb8421f88912d4c10e00d
Den <den34092den@gmail.com>
2020-10-16 23:01:05 +0300

    Backup

---
+++ Lab_7/Lab_7/Lab_7.cpp
@@ -0,0 +1,21 @@
+﻿#include<stdio.h>
+#include<cmath>
+float x, y;
+int main() {
+	printf("x  |  y\n");
+	printf("-------\n");
+	for (int i = 0; i < 4; ++i) {
+		/*General cycle*/
+		for (int j = 0; j < 4; j += 0.25) {
+			/*Nested cycle*/
+			if (j >= 0 && j <= 2)
+				y = j;
+			else if (j > 2 && j <= 3)
+				y = j - 3;
+			else if (j > 3 && j < 4)
+				y = -1;
+			printf("%f  |  %f\n", x, j);
+				x += 0.25;
+		}
+	}
+}
