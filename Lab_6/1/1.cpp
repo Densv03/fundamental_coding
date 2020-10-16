8d602a95d86e14c5fcb4f0c85d583de2d42c11f0
Den <den34092den@gmail.com>
2020-10-16 22:34:49 +0300

    Upload

---
+++ Lab_6/1/1.cpp
@@ -0,0 +1,12 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<cmath>
+int main() {
+	float n /*= 10*/, sum = 0;
+	scanf("%f", &n);
+	for (int i = 1; i <= ; ++i) {
+		sum += ((pow(-1, n)) * ((n + 1) / (n * n + 1)));
+		//printf("%f\n", sum);
+	}
+	printf("%.6f", sum);
+}
---
+++ Lab_6/3/3.cpp
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<ctime>
+#include<iostream>
+int num1, num2, ans, k, mistakes, mark;
+int main() {
+	srand(time(0));
+	for (int i = 0; i < 10; ++i) {
+		num1 = rand() % 9 + 1;
+		num2 = rand() % 9 + 1;
+		printf("%d * %d = ", num1, num2);
+		scanf("%d", &k);
+		if (num1 * num2 != k)mistakes++;
+	}
+	if (mistakes > 4)mark = 1;
+	else mark = 5 - mistakes;
+	printf("Your mark is %d", mark);
+}
