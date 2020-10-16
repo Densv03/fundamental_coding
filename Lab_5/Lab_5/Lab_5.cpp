8e3116ccc65cad857b62841216ef58bf9dbdf10c
Den <den34092den@gmail.com>
2020-10-16 22:06:00 +0300

    Upload

---
+++ Lab_5/Lab_5/Lab_5.cpp
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main() {
+	float x, y;
+	printf("Enter x: ");
+	scanf("%f", &x);
+	printf("Enter y: ");
+	scanf("%f", &y);
+	if (x > 1 || x < -1 || y>1 || y < -1) {
+		printf("Point isn't in area");
+		return 0;
+	}
+	else if (x >= 0) {
+		if (x >= y) //OK
+			printf("Point is in area");
+		else 
+			printf("Point isn't area");
+	}
+	else if (x <= 0) {
+		if (y >= x) //not OK
+			printf("Point is in area");
+		else 
+			printf("Point isn't area");
+	}
+}
---
+++ Lab_5/Lab_5_3/Lab_5_3.cpp
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<Windows.h>
+#include<ctime>
+#include<iostream>
+int main() {
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	int k;
+	scanf("%d", &k);
+	//srand(0);
+	//k = rand() % 1000;
+	if (k % 10 == 0 || k % 10 >= 5 || (k >= 11 && k <= 20))printf("%d грибов\n", k);
+		else if (k % 10 == 1)printf("%d гриб\n", k);
+		else if (k % 10 == 2 || k % 10 == 3 || k % 10 == 4)printf("%d гриба\n", k);
+}
