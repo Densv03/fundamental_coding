65117829746e02c0ddb35bc9a3db9462cc496aa5
Den <den34092den@gmail.com>
2020-10-17 13:00:01 +0300

    Backup

---
+++ Lab_8/1/1.cpp
@@ -0,0 +1,35 @@
+﻿#include<iostream>
+#include<ctime>
+#include<stdio.h>
+
+using namespace std;
+
+short A[100 + 3], n = 100;
+bool outputed = false, check = false;
+
+int main() {
+	/*                  Randomize                              */
+	srand(time(0));
+	/*                  Filling in array                       */
+	for (int i = 0; i < n; ++i)
+		A[i] = rand() % 100 - 50;
+
+	for (int i = 0; i < n; ++i)
+		cout << A[i] << " ";
+	cout << endl;
+	for (int i = 0; i < n; ++i) {
+		if (A[i] > 0 && A[i + 1] < 0 || A[i] < 0 && A[i + 1]>0) {
+			if (!outputed) {
+				cout << i;
+				outputed = true;
+			}
+			check = true;
+		}
+		else {
+			if (check) {
+				cout << i << endl;
+				check = false;
+			}
+		}
+	}
+}
