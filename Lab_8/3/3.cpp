fe8d82ba8cd44de5ef31d5b042471a0d425ae003
Den <den34092den@gmail.com>
2020-10-18 11:08:40 +0300

    Backup

--- Lab_8/3/3.cpp
+++ Lab_8/3/3.cpp
@@ -6,7 +6,7 @@
 #include<utility>
 
 using namespace std;
-int n, k, B[100],pos,t;
+int n, k, B[100],pos,t,C[200];
 pair <int, bool>A[100];
 char action;
 int main() {
@@ -21,40 +21,71 @@ int main() {
 		scanf("%d", &n);
 	}
 
+	// Adding elements in array
 	printf("Введите элементы массива: ");
 	for (int i = 0; i < n; ++i)
 		scanf("%d", &A[i].first);
 
+	// Checking action
 	printf("Введите действие (+ если хотите добавить элементы, - если удалить) ");
 	scanf("%s", &action);
 	if (action == '+') {
+
+		// Quantity elements to add action
 		printf("Сколько элементов хотите добавить? ");
 		scanf("%d", &k);
+
+		// Filling in array for adding elements
+		printf("Какие элементы хотите добавить? ");
+		for (int i = 0; i < k; ++i)
+			scanf("%d", &B[i]);
+
+		// Place for elements
 		printf("В какое место хотите вставить элементы? ");
 		scanf("%d", &pos);
+
+		for (int i = 0; i < pos; ++i)
+			C[i] = A[i].first;
+		for (int i = pos; i < pos + k; ++i)
+			C[i] = B[i - pos];
+		for (int i = pos + k; i < n + k; ++i)
+			C[i] = A[i - pos].first;
+		for (int i = 0; i < n + k; ++i)
+			cout << C[i] << " ";
 	}
 
+	// Checking action
 	else if (action == '-') {
+
+		// Quantity elements to remove
 		printf("Сколько элементов хотите удалить? ");
 		scanf("%d", &k);
+
+		// Place to begin removing
 		printf("С какого элемента хотите начать удаление? ");
 		scanf("%d", &pos);
 		while (k > n) {
 			printf("Введены некорректные данные. Попробуйте ещё раз: ");
 			scanf("%d", &k);
 		}
+
+		// Definition corners of removing
 		A[pos - 1].second = 1;
 		A[pos + k - 2].second = 1;
 		bool u1 = false, u2 = false;
 		for (int i = pos - 1; i < pos + k - 2; ++i)
 			A[i].second = 1;
+
+		// Recreating array
 		for (int i = 0; i < n; ++i) {
 			if (A[i].second == 0) {
 				B[t] = A[i].first;
 					t++;
 			}
 		}
+
+		//Outputting array
 		for (int i = 0; i < t; ++i)
-			cout << B[i] << " ";
+			printf("%d ", B[i]);
 	}
 }
