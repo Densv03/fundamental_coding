3c96e3b9276e1adac956e4e760897aac89170c76
Den <den34092den@gmail.com>
2020-10-18 10:40:54 +0300

    Backup

--- Lab_8/3/3.cpp
+++ Lab_8/3/3.cpp
@@ -3,30 +3,37 @@
 #include<ctime>
 #include<iostream>
 #include<Windows.h>
+#include<utility>
+
 using namespace std;
-int n, A[100], k, B[100],pos;
+int n, k, B[100],pos,t;
+pair <int, bool>A[100];
 char action;
 int main() {
 	SetConsoleOutputCP(1251);
 	SetConsoleCP(1251);
 	srand(time(0));
+
 	printf("Введите количество элементов в массиве: ");
 	scanf("%d", &n);
 	while (n == 0) {
 		printf("Введены некорректные данные. Попробуйте ещё раз: ");
 		scanf("%d", &n);
 	}
+
 	printf("Введите элементы массива: ");
 	for (int i = 0; i < n; ++i)
-		scanf("%d", &A[i]);
+		scanf("%d", &A[i].first);
+
 	printf("Введите действие (+ если хотите добавить элементы, - если удалить) ");
 	scanf("%s", &action);
 	if (action == '+') {
 		printf("Сколько элементов хотите добавить? ");
 		scanf("%d", &k);
-		printf("В какое место хотите вставить элементы?");
+		printf("В какое место хотите вставить элементы? ");
 		scanf("%d", &pos);
 	}
+
 	else if (action == '-') {
 		printf("Сколько элементов хотите удалить? ");
 		scanf("%d", &k);
@@ -36,5 +43,18 @@ int main() {
 			printf("Введены некорректные данные. Попробуйте ещё раз: ");
 			scanf("%d", &k);
 		}
+		A[pos - 1].second = 1;
+		A[pos + k - 2].second = 1;
+		bool u1 = false, u2 = false;
+		for (int i = pos - 1; i < pos + k - 2; ++i)
+			A[i].second = 1;
+		for (int i = 0; i < n; ++i) {
+			if (A[i].second == 0) {
+				B[t] = A[i].first;
+					t++;
+			}
+		}
+		for (int i = 0; i < t; ++i)
+			cout << B[i] << " ";
 	}
 }
