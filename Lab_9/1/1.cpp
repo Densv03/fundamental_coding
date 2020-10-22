cb0dc2de927dfbbfa6b0ae75ac090358175172db
Den <den34092den@gmail.com>
2020-10-22 13:21:49 +0300

    Upload

---
+++ Lab_9/1/1.cpp
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+int main() {
+	int A[10][10], n = 9;
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) {
+			A[i][j] = j;
+		}
+	}
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j)
+			cout << A[i][j];
+		cout << endl;
+	}
+}
