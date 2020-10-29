ea6312bab5c067a9d328d749959e67e32b6f7ea4
Den <den34092den@gmail.com>
2020-10-29 12:03:25 +0200

    Lab 5_3 was completed

--- Lab_5/Lab_5_3/Lab_5_3.cpp
+++ Lab_5/Lab_5_3/Lab_5_3.cpp
@@ -6,11 +6,22 @@
 int main() {
 	SetConsoleOutputCP(1251);
 	SetConsoleCP(1251);
-	int k;
-	scanf("%d", &k);
-	//srand(0);
-	//k = rand() % 1000;
-	if (k % 10 == 0 || k % 10 >= 5 || (k >= 11 && k <= 20))printf("%d грибов\n", k);
+	int k=0;
+	
+	while (k != -1) {
+		printf("Enter number of mashrooms: ");
+		scanf("%d", &k);
+		if (k % 10 == 0 || k % 10 >= 5 || (k >= 11 && k <= 20))printf("%d грибов\n", k);
 		else if (k % 10 == 1)printf("%d гриб\n", k);
 		else if (k % 10 == 2 || k % 10 == 3 || k % 10 == 4)printf("%d гриба\n", k);
+	}
+	/*
+	srand(time(0));
+	for (int i = 0; i < 100; ++i) {
+		k = rand() % 500 + 1;
+		if (k % 10 == 0 || k % 10 >= 5 || (k >= 11 && k <= 20))printf("%d грибов\n", k);
+		else if (k % 10 == 1)printf("%d гриб\n", k);
+		else if (k % 10 == 2 || k % 10 == 3 || k % 10 == 4)printf("%d гриба\n", k);
+	}
+	*/
 }
