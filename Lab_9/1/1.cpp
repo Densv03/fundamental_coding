b6bfdc8d424b43ac9f4600677ca184db5db59439
Den <den34092den@gmail.com>
2020-11-08 23:55:33 +0200

    Lab 9 was completed

--- Lab_9/1/1.cpp
+++ Lab_9/1/1.cpp
@@ -5,6 +5,8 @@ using namespace std;
 int A[10][10], n = 9, counter = 1, k;
 int main() {
 	int shift = 0;
+	printf("Enter n (n - matrix size): ");
+	scanf("%d", &n);
 	if (n % 2 != 0)A[n / 2][n / 2] = n * n;
 	// General cycle
 	for (int h = 0; h < n / 2; ++h) {
@@ -17,12 +19,30 @@ int main() {
 		}
 		// Filling in bottom
 
+		int i = n - shift - 1;
+		for (int j = shift + 1; j < n - shift; ++j) {
+			A[i][j] = counter;
+			counter++;
+		}
+		// Filling in right side
 
+		int j = n - shift - 1;
+		for (int i = n - shift - 2; i >= shift; --i) {
+			A[i][j] = counter;
+			counter++;
+		}
+		// Filling in top side
+
+		i = shift;
+		for (j = n - shift - 2; j >= shift + 1; j--) {
+			A[i][j] = counter;
+			counter++;
+		}
 		shift++;
 	}
 	for (int i = 0; i < n; ++i) {
 		for (int j = 0; j < n; ++j)
-			cout << A[i][j] << " ";
-		cout << endl;
+			printf("%d ", A[i][j]);
+		printf("\n");
 	}
 }
