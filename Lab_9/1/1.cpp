56ecefad4b3f95eb9798bda3a73134d0b46b7fd3
Den <den34092den@gmail.com>
2020-11-08 23:15:07 +0200

    Backuped Lab 9

--- Lab_9/1/1.cpp
+++ Lab_9/1/1.cpp
@@ -4,26 +4,20 @@
 using namespace std;
 int A[10][10], n = 9, counter = 1, k;
 int main() {
-	/*for (int i = 0; i < n; ++i) {
-		for (int j = 0; j < n; ++j) {
-			A[j][i] = counter;
+	int shift = 0;
+	if (n % 2 != 0)A[n / 2][n / 2] = n * n;
+	// General cycle
+	for (int h = 0; h < n / 2; ++h) {
+		// Filling in left side
+
+		for (int i = shift; i < n - shift; ++i) {
+			int j = shift;
+			A[i][j] = counter;
 			counter++;
 		}
-	}*/
-	int shift = 0;
-	if (n % 2 == 1)A[n / 2][n / 2] = n * n;
-	k = (n % 2 == 1) ? n / 2 + 1 : n / 2;
+		// Filling in bottom
 
-	// General cycle
-	for (int i = 0; i < k; ++i) {
 
-		// Cycles for left part
-		for (int j = shift; j < shift + 1; ++j) {
-			for (int l = shift; l < n - 2 * shift+1; l++) {
-				A[l][j] = counter;
-				counter++;
-			}
-		}
 		shift++;
 	}
 	for (int i = 0; i < n; ++i) {
