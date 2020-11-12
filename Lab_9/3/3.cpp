7e93c4159072ffe459fca82b63bb53ae5b495c4a
Den <den34092den@gmail.com>
2020-11-12 14:59:35 +0200

    Lab 9_3

--- Lab_9/3/3.cpp
+++ Lab_9/3/3.cpp
@@ -3,8 +3,8 @@
 #include<iostream>
 #include<ctime>
 using namespace std;
+int n, m, o, p, checker = 0, A[50][50], B[50][50], C[50][50];
 int main() {
-	int n, m, o, p, checker = 0, A[50][50], B[50][50], C[50][50];
 	printf("Do you want to enter matrix size from keyboard? Enter either 1 if you want or 2 if you don't: ");
 	scanf("%d", &checker);
 	while (checker != 1 && checker != 2) {
@@ -41,17 +41,17 @@ int main() {
 	}
 	else {
 		srand(time(0));
-		m = rand() % 10 + 1;
-		n = rand() % 10 + 1;
+		m = rand() % 4 + 1;
+		n = rand() % 4 + 1;
 		o = n;
-		p = rand() % 10 + 1;
+		p = rand() % 4 + 1;
 		printf("Generates matrix sizes are:\nm = %d\nn = %d\no = %d\np = %d\n", m, n, o, p);
 		for (int i = 0; i < m; i++)
 			for (int j = 0; j < n; j++)
-				A[i][j] = rand() % 30 + 1;
+				A[i][j] = rand() % 10 + 1;
 		for (int i = 0; i < o; i++)
 			for (int j = 0; j < p; ++j)
-				B[i][j] = rand() % 30 + 1;
+				B[i][j] = rand() % 10 + 1;
 		printf("First matrix that we have:\n");
 		for (int i = 0; i < m; ++i) {
 			for (int j = 0; j < n; ++j)
@@ -64,5 +64,27 @@ int main() {
 				printf("%d ", B[i][j]);
 			printf("\n");
 		}
+	}
+	/*A[0][0] = 1, A[0][1] = 2, A[0][2] = 3, A[1][0] = 4, A[1][1] = 5, A[1][2] = 6, A[2][0] = 7, A[2][1] = 8, A[2][2] = 9;
+	B[0][0] = 1, B[0][1] = 2, B[1][0] = 3, B[1][1] = 4, B[2][0] = 5, B[2][1] = 6;
+	n = 3, m = 3, o = 3, p = 2;*/
+	// Starting calculate
+
+	int k = 0;
+	for (int i = 0; i < m; ++i) {
+		for (int j = 0; j < p; ++j) {
+			while (k!=m) {
+				C[i][j] += A[i][k] * B[k][j];
+				k++;
+			}
+			k = 0;
+		}
+	}
+
+	cout << "Result: " << endl;
+	for (int i = 0; i < m; ++i) {
+		for (int j = 0; j < p; ++j)
+			printf("%d ", C[i][j]);
+		printf("\n");
 	}
 }
