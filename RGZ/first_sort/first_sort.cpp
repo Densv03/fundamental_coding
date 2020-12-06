da2d34b13212b9e1061d0b6c738bb35a4bd9f6a4
Den <den34092den@gmail.com>
2020-12-06 17:54:39 +0200

    update gnome sort

--- RGZ/first_sort/first_sort.cpp
+++ RGZ/first_sort/first_sort.cpp
@@ -29,8 +29,8 @@ int main() {
 	for (int j = 0; j < N; ++j) {
 		for (int l = 0; l < K; ++l)
 			A[l] = A_copy[l];
-		sort(A, A + K);
-		reverse(A, A + K);
+		//sort(A, A + K);
+		//reverse(A, A + K);
 		auto start= chrono::high_resolution_clock::now();
 		gnome_sort(A);
 		auto end = chrono::high_resolution_clock::now();
@@ -40,10 +40,10 @@ int main() {
 	cout << endl << "Sorted array:" << endl;
 	for (int i = 0; i < K; ++i)
 		cout << A[i] << " ";
-	cout << endl << "Duration array:" << endl;
-	for (int i = 0; i < N; ++i) {
-		cout << "Duration " << i << " = " << dur_arr[i] << "s\n";
-	}
+	cout << endl;
+	/*cout << "Duration array: " << endl;
+	for (int i = 0; i < N; ++i)
+		cout << "Duration " << i << " = " << dur_arr[i] << "s\n";*/
 	sort(dur_arr, dur_arr + N);
 	for (int i = 100; i < N - 100; ++i)
 		aver_dur += dur_arr[i];
